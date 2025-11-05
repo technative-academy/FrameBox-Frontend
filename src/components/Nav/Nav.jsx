fcb232f953b12f1c664f70bc3e0f21222be4ad07
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-05 15:46:26 +0000

    add if statement for showing login/signup

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,31 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Nav.module.css";
 
-function SiteNav() {
-  const navLinks = [
-    { label: "Home", url: "/" },
-    { label: "Movies", url: "/Movies" },
-    { label: "SignUp", url: "/SignUp" },
-    { label: "Login", url: "/Login" },
-  ];
-  return (
-    <div className={styles.wrapper}>
-      <nav className={styles.links}>
-        {navLinks.map((navLink) => (
-          <NavLink
-            key={navLink.url}
-            to={navLink.url}
-            className={({ isActive }) =>
-              isActive ? styles.activeLink : styles.inactiveLink
-            }
-          >
-            {navLink.label}
-          </NavLink>
-        ))}
-      </nav>
-    </div>
-  );
+function SiteNav({ type }) {
+    const navLinks = [
+        { label: "Home", url: "/" },
+        { label: "Movies", url: "/Movies" },
+        type === "login"
+            ? { label: "Login", url: "/Login" }
+            : { label: "SignUp", url: "/SignUp" },
+    ];
+    return (
+        <div className={styles.wrapper}>
+            <nav className={styles.links}>
+                {navLinks.map((navLink) => (
+                    <NavLink
+                        key={navLink.url}
+                        to={navLink.url}
+                        className={({ isActive }) =>
+                            isActive ? styles.activeLink : styles.inactiveLink
+                        }
+                    >
+                        {navLink.label}
+                    </NavLink>
+                ))}
+            </nav>
+        </div>
+    );
 }
 
 export default SiteNav;
