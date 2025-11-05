42a14b03bcd3156dc2192f9f79c8daaaaf7f3a82
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-05 12:01:37 +0000

    add new root page

---
+++ src/components/root/Root.jsx
@@ -0,0 +1,17 @@
+import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
+import MainContent from "../MainContent/MainContent";
+
+function Root() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default Root;
