387ff9a9ae84d80a400b232a5df61234e140373a
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-05 15:47:03 +0000

    remove import not needed

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
 
 function MainContent() {
-  return (
-    <div className>
-      <Outlet />
-    </div>
-  );
+    return (
+        <div className>
+            <Outlet />
+        </div>
+    );
 }
 
 export default MainContent;
