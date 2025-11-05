a958eb5262f37d22d8509032efdf22cee3ce0bab
Nathan Hor <51942735+NathanHor22@users.noreply.github.com>
2025-11-05 16:31:31 +0000

    Carousel is made more mobile responsive and scalable for other parts of website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,14 @@ import Carousel from '../components/Carousel/Carousel';
 function Home() {
   return (
     <div className="bg-amber-50 min-h-screen">
-      <h1 className="text-red-600 text-3xl text-center py-4">Home Page</h1>
+      <h1 className="text-red-600 text-3xl text-start py-4">Trending</h1>
       <Carousel />
+      <div>
+        <h1 className="text-red-600 text-3xl text-start py-4">My movies</h1>
+        <Carousel />
+      </div>
     </div>
+
   );
 }
 
