8467b689b276ed5c3853e6cf7883a821cbc0ecb4
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-05 12:01:25 +0000

    add new nav page

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Nav.module.css";
+
+function SiteNav() {
+  const navLinks = [
+    { label: "Home", url: "/" },
+    { label: "Movies", url: "/Movies" },
+    { label: "SignUp", url: "/SignUp" },
+    { label: "Login", url: "/Login" },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <nav className={styles.links}>
+        {navLinks.map((navLink) => (
+          <NavLink
+            key={navLink.url}
+            to={navLink.url}
+            className={({ isActive }) =>
+              isActive ? styles.activeLink : styles.inactiveLink
+            }
+          >
+            {navLink.label}
+          </NavLink>
+        ))}
+      </nav>
+    </div>
+  );
+}
+
+export default SiteNav;
