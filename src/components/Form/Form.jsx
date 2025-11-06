a419f3d87c11f5f3fc074bc066687b36d54b6f9c
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-06 11:08:34 +0000

    add border for inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,38 @@
 function Form({ type }) {
     return (
-        <div className="flex items-center justify-center  text-2xl ">
-            <form className="flex flex-col gap-4 mx-auto p-16 rounded-2xl shadow-[0px_7px_29px_0px_rgba(100,100,111,0.2)]">
-                <input type="text" placeholder="Username" />
+        <div className="flex items-center justify-center text-xl ">
+            <form className="flex flex-col gap-4 mx-auto mt-4 p-12 rounded-2xl shadow-[0px_7px_29px_0px_rgba(100,100,111,0.2)]">
+                <h1 className="text-2xl text-center">
+                    {type === "signup" ? "Sign Up" : "Login"}
+                </h1>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    className="border border-gray-300 rounded-md px-3 py-2"
+                />
                 {type === "signup" && (
-                    <input type="email" placeholder="Email" />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="border border-gray-300 rounded-md px-3 py-2"
+                    />
                 )}
 
-                <input type="password" placeholder="Type Password" />
+                <input
+                    type="password"
+                    placeholder="Type Password"
+                    className="border border-gray-300 rounded-md px-3 py-2"
+                />
                 {type === "signup" && (
-                    <input type="password" placeholder="Repeat Password" />
+                    <input
+                        type="password"
+                        placeholder="Repeat Password"
+                        className="border border-gray-300 rounded-md px-3 py-2"
+                    />
                 )}
                 <button
                     type="submit"
-                    className="bg-black text-white p-2 mt-2 rounded-md w-24"
+                    className="bg-black text-white p-2 mx-auto rounded-md w-24"
                 >
                     Submit
                 </button>
