1af2567d191520c8db01eab2ef2edca5b491bd67
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-06 11:23:18 +0000

    change padding for titles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Carousel from "../components/carousel/carousel";
 function Home() {
     return (
         <div className="bg-amber-50 min-h-screen">
-            <h1 className="text-black text-3xl text-start py-4 ml-4">
+            <h1 className="text-black text-3xl text-start py-2 ml-4">
                 Trending
             </h1>
             <Carousel />
