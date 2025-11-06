aff6299ef1380304e2235a43dd22c3a1124d4289
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-06 12:02:32 +0000

    have the footer always stay at the bottom

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import Form from "../components/Form/Form";
 
 function Login() {
     return (
-        <div>
+        <div className="flex-1 flex flex-col lg:min-h-full">
             <Form type="login" />
         </div>
     );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import Form from "../components/Form/Form";
 
 function SignUp() {
     return (
-        <div>
+        <div className="flex-1 flex flex-col lg:min-h-full">
             <Form type="signup" />
         </div>
     );
