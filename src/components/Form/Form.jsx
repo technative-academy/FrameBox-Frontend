b2afae7e89233d4e255a690d5b5ed02757b4213a
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-06 12:01:49 +0000

    have the footer always stay at the bottom

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 function Form({ type }) {
     return (
-        <div className="flex items-center justify-center text-xl ">
+        <div className="flex items-center justify-center text-xl lg:mb-16 flex-1">
             <form className="flex flex-col gap-4 mx-auto mt-4 p-8 rounded-2xl shadow-[0px_7px_29px_0px_rgba(100,100,111,0.2)]">
                 <h1 className="text-2xl text-center">
                     {type === "signup" ? "Sign Up" : "Login"}
