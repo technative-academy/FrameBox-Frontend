b088298125ccb7f65a85cc580c6a2fe0ebb66dd2
Nathan Hor <nathanhor2001@gmail.com>
2025-11-07 09:00:00 +0000

    Added comments to make playlistHelpers more easier to understand

--- src/playlistHelpers.jsx
+++ src/playlistHelpers.jsx
@@ -1,6 +1,8 @@
 import { movies } from './data-test.jsx';
 import { playlists } from './playlist-data.jsx';
 
+//Finds playslist by ID and returns associated movies
+//returns an array of movie objects
 export const getMoviesForPlaylist = (playlistId) => {
   const playlist = playlists.find(p => p.id === playlistId);
   if (!playlist) return [];
@@ -10,6 +12,7 @@ export const getMoviesForPlaylist = (playlistId) => {
   ).filter(Boolean);
 };
 
+//add movies that isn't already in the playlist
 export const addMovieToPlaylist = (playlistId, movieId) => {
   const playlist = playlists.find(p => p.id === playlistId);
   if (playlist && !playlist.movieIds.includes(movieId)) {
@@ -17,6 +20,7 @@ export const addMovieToPlaylist = (playlistId, movieId) => {
   }
 };
 
+//removes movie from the playlist
 export const removeMovieFromPlaylist = (playlistId, movieId) => {
   const playlist = playlists.find(p => p.id === playlistId);
   if (playlist) {
@@ -24,6 +28,7 @@ export const removeMovieFromPlaylist = (playlistId, movieId) => {
   }
 };
 
+//checks if a movie is in the playlist
 export const isMovieInPlaylist = (playlistId, movieId) => {
   const playlist = playlists.find(p => p.id === playlistId);
   return playlist ? playlist.movieIds.includes(movieId) : false;
