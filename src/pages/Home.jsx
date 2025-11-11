92b193c32d6e25608659584b62377e7a6fc09b9f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-11 19:57:22 +0000

    change import link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
-import Carousel from '../components/carousel/carousel';
-import PlaylistCarousel from '../components/Carousel/playlistcarousel';
+import Carousel from "../components/carousel/carousel";
+import PlaylistCarousel from "../components/carousel/playlistcarousel";
 
 function Home() {
-  return (
-    <div className="bg-amber-50 min-h-screen pb-16 sm:pb-8">
-      <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 px-2 pt-2">Trending Now</h2>
-      <Carousel />
-      <div className="mt-2">
-        <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 px-2">Your Collection</h2>
-        <PlaylistCarousel />
-      </div> 
-    </div>
-  );
+    return (
+        <div className="bg-amber-50 min-h-screen pb-16 sm:pb-8">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 px-2 pt-2">
+                Trending Now
+            </h2>
+            <Carousel />
+            <div className="mt-2">
+                <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 px-2">
+                    Your Collection
+                </h2>
+                <PlaylistCarousel />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
