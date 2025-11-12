188744e3ac503ca14decd4bf59711fc772454b1c
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-12 15:34:39 +0000

    add more playlist examples for carousel

--- src/playlist-data.jsx
+++ src/playlist-data.jsx
@@ -1,20 +1,32 @@
 export const playlists = [
-  {
-    id: 1,
-    playlist_name: "My Favorites",
-    img: "/playlist-cover-1.PNG",
-    movieIds: [1, 3, 5]
-  },
-  {
-    id: 2,
-    playlist_name: "Action Collection", 
-    img: "/playlist-cover-2.PNG",
-    movieIds: [2, 4]
-  },  
-  {
-    id: 3,
-    playlist_name: "Watch Later",
-    img: "/playlist-cover-3.PNG",
-    movieIds: [1, 2, 3, 4, 5]
-  }
+    {
+        id: 1,
+        playlist_name: "My Favorites",
+        img: "/playlist-cover-1.PNG",
+        movieIds: [1, 3, 5],
+    },
+    {
+        id: 2,
+        playlist_name: "Action Collection",
+        img: "/playlist-cover-2.PNG",
+        movieIds: [2, 4],
+    },
+    {
+        id: 3,
+        playlist_name: "Adventure Collection",
+        img: "/playlist-cover-3.PNG",
+        movieIds: [1, 2, 3, 4, 5],
+    },
+    // {
+    //     id: 4,
+    //     playlist_name: "Comedy Collection",
+    //     img: "/playlist-cover-3.PNG",
+    //     movieIds: [1, 2, 3, 4, 5],
+    // },
+    // {
+    //     id: 5,
+    //     playlist_name: "Romance Collection",
+    //     img: "/playlist-cover-2.PNG",
+    //     movieIds: [2, 4],
+    // },
 ];
