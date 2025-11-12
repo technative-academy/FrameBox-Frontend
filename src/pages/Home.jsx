2ad33cbd45db55ab8810fa27710e014c0b18628d
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-12 15:34:09 +0000

    center title and carousel stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,19 @@ import PlaylistCarousel from "../components/carousel/playlistcarousel";
 function Home() {
     return (
         <div className="bg-amber-50 min-h-screen pb-16 sm:pb-8 w-screen">
-            <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 px-4 pt-2">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 text-center">
                 Trending Now
             </h2>
-            <Carousel />
+            <div className="flex justify-center">
+                <Carousel />
+            </div>
             <div className="mt-2">
-                <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 px-2">
+                <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 text-center">
                     Your Collection
                 </h2>
-                <PlaylistCarousel />
+                <div className="flex justify-center">
+                    <PlaylistCarousel />
+                </div>
             </div>
         </div>
     );
