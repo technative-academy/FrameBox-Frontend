e21f5407408a1f7262c7ebf73b78ee3cc4dc2d16
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-13 12:41:26 +0000

    remove un-used imports of files

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/authSlice";
-import thingsReducer from "./slices/thingsSlice";
 import usersReducer from "./slices/usersSlice";
 
 const store = configureStore({
