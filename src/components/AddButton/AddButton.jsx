1a98abb65b1679edde77130752f8a085b21dee50
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-19 13:51:38 +0000

    simplifier addbutton

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -7,7 +7,7 @@ function AddButton() {
             }}
             className="z-10 bg-black/70 hover:bg-black/90 text-white p-2 rounded-l"
         >
-            +
+            ...
         </button>
     );
 }
