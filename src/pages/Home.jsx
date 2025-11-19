0d78797d3209a047207371a318cf99d5f2b47b6f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-19 15:27:15 +0000

    add playlist form and button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import Carousel from "../components/carousel/carousel";
 import PlaylistCarousel from "../components/carousel/playlistcarousel";
 import AddButton from "../components/AddButton/AddButton";
+import PlayListForm from "../components/PlaylistForm/PlaylistForm";
+import { useState } from "react";
+
 function Home() {
+    const [showPlayForm, setShowPlayForm] = useState(false);
+
+    const onAddButtonClick = () => {
+        setShowPlayForm(!showPlayForm);
+    };
+    const buttonName = "Create Playlist";
+
     return (
         <div className="bg-amber-50 min-h-screen pb-16 sm:pb-8 w-screen">
             <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 text-center">
@@ -15,11 +25,19 @@ function Home() {
                 <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-3 text-center">
                     Your Collection
                 </h2>
-                <div className="relative flex justify-end mb-4 pr-2 sm:pr-10">
-                    <AddButton />
+                <div className=" flex justify-end mb-4 pr-2 sm:pr-10">
+                    <AddButton
+                        nameOfButton={buttonName}
+                        onClick={onAddButtonClick}
+                    />
                 </div>
-                <div className="flex justify-center">
-                    <PlaylistCarousel />
+                <div>
+                    <div className="relative z-100 ">
+                        {showPlayForm && <PlayListForm />}
+                    </div>
+                    <div className="flex justify-center">
+                        <PlaylistCarousel />
+                    </div>
                 </div>
             </div>
         </div>
