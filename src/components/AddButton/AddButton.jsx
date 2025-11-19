1b245d8928617b0be2cc773ceb67f201cf868864
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-19 12:35:30 +0000

    button fix to showcase on playlist carousel

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -5,7 +5,7 @@ function AddButton({ type }) {
                 e.stopPropagation();
                 setShowPlaylistMenu(!showPlaylistMenu);
             }}
-            className="absolute top-2 right-2 bg-black/70 hover:bg-black/90 text-white w-8 h-8 flex items-center justify-center rounded-full opacity-0 group-hover/card:opacity-100 transition-opacity"
+            className="absolute right-2 sm:right-0 top-1/2 -translate-y-1/2 z-10 bg-black/70 hover:bg-black/90 text-white p-2 rounded-l sm:opacity-0 sm:group-hover:opacity-100 transition-opacity"
         >
             +
         </button>
