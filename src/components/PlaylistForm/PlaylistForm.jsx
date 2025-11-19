3bbab88a010f533d6f795709763d21b484c73c4e
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-19 15:22:14 +0000

    add form set-up and design

--- src/components/PlaylistForm/PlaylistForm.jsx
+++ src/components/PlaylistForm/PlaylistForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { addPlaylists } from "../../slices/playListFormSlice.jsx";
+import { useSelector, useDispatch } from "react-redux";
 
-function playListForm() {
+function PlayListForm() {
     const dispatch = useDispatch();
     const movies = useSelector((state) => state.movies.items);
     const status = useSelector((state) => state.movies.status);
@@ -9,44 +10,54 @@ function playListForm() {
     const [playlistName, setPlaylistName] = useState("");
     const [playlistDesc, setPlaylistDesc] = useState("");
     const handleSubmit = (e) => {
-        console.log("form submitted");
         e.preventDefault();
+        dispatch(
+            addPlaylists({
+                title: playlistName,
+                summary: playlistDesc,
+            })
+        );
     };
 
     return (
-        <div className="flex items-center justify-center text-xl lg:mb-16 flex-1">
-            <h1 className="text-2xl text-center">Create Playlist</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    playlist Name
-                    <input
-                        type="text"
-                        placeholder="Playlist Name"
-                        className="border border-gray-300 rounded-md px-3 py-2"
-                        value={playlistName}
-                        onChange={(e) => setPlaylistName(e.target.value)}
-                    />
-                </label>
-                <label>
-                    playlist Descritpion
-                    <input
-                        type="text"
-                        placeholder="Playlist Name"
-                        className="border border-gray-300 rounded-md px-3 py-2"
-                        value={playlistDesc}
-                        onChange={(e) => setPlaylistDesc(e.target.value)}
-                    />
-                </label>
-
-                <button
-                    type="submit"
-                    className="bg-black text-white p-2 mx-auto rounded-md w-24"
+        <div className="absolute flex justify-end items-center pr-4 bg-red-100 ">
+            <div className="flex flex-col items-center justify-center text-xl lg:mb-16 ">
+                <h1 className="text-2xl text-center">Create Playlist</h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className="  justify-center items-center  text-center gap-4 p-6 rounded-md flex flex-col"
                 >
-                    Submit
-                </button>
-            </form>
+                    <label className="">
+                        Playlist Name
+                        <input
+                            type="text"
+                            placeholder=" Name"
+                            className="border border-black flex justify-end rounded-md px-1 py-1"
+                            value={playlistName}
+                            onChange={(e) => setPlaylistName(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Playlist Descritpion
+                        <input
+                            type="text"
+                            placeholder=" Descritpion"
+                            className="border border-black flex justify-end rounded-md px-1 py-1"
+                            value={playlistDesc}
+                            onChange={(e) => setPlaylistDesc(e.target.value)}
+                        />
+                    </label>
+
+                    <button
+                        type="submit"
+                        className="bg-black justify-center flex text-white p-2 mx-auto rounded-md w-24"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
 
-export default playListForm;
+export default PlayListForm;
