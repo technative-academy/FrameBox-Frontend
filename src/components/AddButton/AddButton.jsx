8054ce63d8d7537b0682ece1ef047547e6cf0992
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-19 12:59:18 +0000

    simplify add button

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,11 +1,11 @@
-function AddButton({ type }) {
+function AddButton() {
     return (
         <button
             onClick={(e) => {
                 e.stopPropagation();
                 setShowPlaylistMenu(!showPlaylistMenu);
             }}
-            className="absolute right-2 sm:right-0 top-1/2 -translate-y-1/2 z-10 bg-black/70 hover:bg-black/90 text-white p-2 rounded-l sm:opacity-0 sm:group-hover:opacity-100 transition-opacity"
+            className="z-10 bg-black/70 hover:bg-black/90 text-white p-2 rounded-l"
         >
             +
         </button>
