21d07504296e381b8e3dcc4cbfc3f4d43ff38f73
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-20 16:57:04 +0000

    add new addToPlaylist slice

---
+++ src/slices/addToPlayListSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import makeApiRequest from "../services/apiService";
+
+// Async thunk to fetch playlists
+export const addToPlayLists = createAsyncThunk(
+    "playlists/addToPlaylists",
+    async (playlist) => {
+        const response = await makeApiRequest("playlists", {
+            method: "POST",
+            body: JSON.stringify(playlist),
+        });
+
+        return response;
+    }
+);
+
+const addToPlayListSlice = createSlice({
+    name: "playlists",
+    initialState: {
+        items: [],
+        status: "idle",
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(addToPlayLists.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(addToPlayLists.fulfilled, (state, action) => {
+                state.items = action.payload;
+                state.status = "succeeded";
+            })
+            .addCase(addToPlayLists.rejected, (state) => {
+                state.status = "failed";
+            });
+    },
+});
+
+export default addToPlayListSlice.reducer;
