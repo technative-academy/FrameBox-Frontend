d6a6a06bd47789cec10d8b7425489bc0ee65425d
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-11-25 23:21:21 +0000

    change look of hover on imgs

--- src/components/PlaylistForm/PlaylistForm.jsx
+++ src/components/PlaylistForm/PlaylistForm.jsx
@@ -21,19 +21,19 @@ function PlayListForm({ onCancel }) {
     };
 
     return (
-        <div className="absolute flex justify-end items-center pr-4 bg-red-100 ">
-            <div className="flex flex-col items-center justify-center text-xl lg:mb-16 ">
+        <div className="absolute flex justify-start items-center pr-4 bg-zinc-700 rounded-lg">
+            <div className="flex flex-col items-center justify-center text-xl lg:mb-16">
                 <h1 className="text-2xl text-center">Create Playlist</h1>
                 <form
                     onSubmit={handleSubmit}
-                    className="justify-center items-center text-center gap-4 p-6 rounded-md flex flex-col"
+                    className="justify-center items-center text-center gap-4 p-6 flex  flex-col "
                 >
                     <label className="">
                         Playlist Name
                         <input
                             type="text"
                             placeholder=" Name"
-                            className="border border-black flex justify-end rounded-md px-1 py-1"
+                            className="border border-white flex justify-end rounded-lg px-1 py-2"
                             value={playlistName}
                             onChange={(e) => setPlaylistName(e.target.value)}
                         />
@@ -43,19 +43,28 @@ function PlayListForm({ onCancel }) {
                         <input
                             type="text"
                             placeholder=" Descritpion"
-                            className="border border-black flex justify-end rounded-md px-1 py-1"
+                            className="border border-white flex justify-end rounded-lg px-1 py-1"
                             value={playlistDesc}
                             onChange={(e) => setPlaylistDesc(e.target.value)}
                         />
                     </label>
 
-                    <button
-                        type="submit"
-                        onClick={onCancel}
-                        className="bg-black justify-center flex text-white p-2 mx-auto rounded-md w-24"
-                    >
-                        Submit
-                    </button>
+                    <div className="flex gap-3 justify-center mt-5">
+                        <button
+                            type="submit"
+                            onClick={onCancel}
+                            className="bg-black justify-center flex text-white p-2 mx-auto rounded-md w-24"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="sumbit"
+                            onClick={onCancel}
+                            className="px-4 py-2 bg-gray-400 text-white rounded-md"
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>
